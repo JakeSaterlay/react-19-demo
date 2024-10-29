ec44419828854cc8cca99db640d4236eb80e5033
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2024-10-29 10:47:41 +0000

    final test commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,6 @@ export default async function Home() {
   const users = await db.select().from(usersTable).all();
   console.log(users);
   return (
-    <div>Test 1</div>
+    <div>Test</div>
   );
 }
