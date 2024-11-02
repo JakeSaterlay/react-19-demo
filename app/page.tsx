1f701cab953d190a80d3884498c5675a0dabab4e
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2024-11-02 13:38:54 +0000

    mapped through users

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { usersTable } from "@/db/schema";
 
 export default async function Home() {
   const users = await db.select().from(usersTable).all();
-  return <div className="text-blue-500">Testyyyyy</div>;
+  return <div className="text-blue-500">{users.map((user) => user.name)}</div>;
 }
